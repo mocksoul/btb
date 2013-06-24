272b568e08a2c4f6fc80937bfb3b3a7e74504971
Vadim Fint <mocksoul@gmail.com>
2013-06-24 16:00:39 +0400

    feature: use setuptools >= 0.7 in setup.py
    --HG--
    branch : feature

--- setup.py
+++ setup.py
@@ -6,46 +6,69 @@ try:
     from setuptools import setup
     from setuptools.command.test import test as TestCommand
 
-    class PyTestCommand(TestCommand):
-        user_options = [
-            ('opts=', 'o', 'Options for pytest'),
-        ]
 
-        def initialize_options(self):
-            self.opts = ''
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+from setuptools.command.sdist import sdist as SourceDistCommand
 
-        def finalize_options(self):
-            import shlex
-            self.opts_str = self.opts
-            self.opts = shlex.split(self.opts)
 
-        def run(self):
-            if self.distribution.install_requires:
-                self.distribution.fetch_build_eggs(self.distribution.install_requires)
-            if self.distribution.tests_require:
-                self.distribution.fetch_build_eggs(self.distribution.tests_require)
+class PyTestCommand(TestCommand):
+    user_options = [
+        ('opts=', 'o', 'Options for pytest'),
+    ]
 
-            self.with_project_on_sys_path(self.run_pytest)
+    def initialize_options(self):
+        self.opts = ''
 
-        def run_pytest(self):
-            import sys
-            import pytest
-            import os
+    def finalize_options(self):
+        import shlex
+        self.opts_str = self.opts
+        self.opts = shlex.split(self.opts)
 
-            if os.fork():
-                status = os.wait()[1]
-                if os.WIFEXITED(status):
-                    raise SystemExit(os.WEXITSTATUS(status))
-                else:
-                    self.announce('Killed by signal: %d' % (os.WSTOPSIG(status), ))
-                    raise SystemExit(1)
+    def run(self):
+        if self.distribution.install_requires:
+            self.distribution.fetch_build_eggs(self.distribution.install_requires)
+        if self.distribution.tests_require:
+            self.distribution.fetch_build_eggs(self.distribution.tests_require)
+
+        self.with_project_on_sys_path(self.run_pytest)
+
+    def run_pytest(self):
+        import sys
+        import pytest
+        import os
+
+        if os.fork():
+            status = os.wait()[1]
+            if os.WIFEXITED(status):
+                raise SystemExit(os.WEXITSTATUS(status))
             else:
-                sys.argv[:] = ['setup.py test'] + self.opts
-                self.announce('Running pytest %s' % (self.opts_str, ))
-                raise SystemExit(pytest.main())
+                self.announce('Killed by signal: %d' % (os.WSTOPSIG(status), ))
+                raise SystemExit(1)
+        else:
+            sys.argv[:] = ['setup.py test'] + self.opts
+            self.announce('Running pytest %s' % (self.opts_str, ))
+            raise SystemExit(pytest.main())
 
-except ImportError:
-    from distutils.core import setup  # NOQA
+
+class MySourceDistCommand(SourceDistCommand):
+    def make_distribution(self):
+        import os
+        import textwrap
+
+        self.filelist.extend(['tools/setup.py', 'tools/ez_setup.py'])
+        self.filelist.extend(['setup.py'])
+        open('setup.py', 'wb').write(textwrap.dedent('''
+            import os
+            import sys
+            sys.path.insert(0, 'tools')
+            execfile('tools/setup.py')
+        ''').strip())
+        #os.symlink('tools/setup.py', 'setup.py')
+        try:
+            return SourceDistCommand.make_distribution(self)
+        finally:
+            os.unlink('setup.py')
 
 
 try:
@@ -81,6 +104,7 @@ setup(
     package_dir={'btb': 'src'},
     cmdclass={
         'test': PyTestCommand,
+        'sdist': MySourceDistCommand,
     },
     classifiers=[
         'Development Status :: 1 - Planning',
