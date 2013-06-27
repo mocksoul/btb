15f35dba048b7e3a29d49da1307725216a19352d
Vadim Fint <mocksoul@gmail.com>
2013-06-27 20:21:06 +0400

    feature: move historic options from sync to archive
    refs: #1
    
    --HG--
    branch : feature

--- src/ui/console/commands/archive.py
+++ src/ui/console/commands/archive.py
@@ -20,6 +20,24 @@ def create_parser(parser):
         config='x.y'
     )
 
+    historic_group = parser.add_argument_group(
+        title='Backup history management'
+    )
+    historic_group.add_argument(
+        '--max-age', metavar='AGE',
+        default='1w',
+        help=textwrap.dedent('''
+            Max snapshots age.
+            Examples:
+             - 30 (30 days)
+             - 2d10h (2 days 10 hours)
+             - 10m (invalid! cant distinguish between months and minutes)
+             - 10mi (10 minutes)
+             - 10mo (10 months)
+             - 10s1h (stupid form, but valid: 1 hour 10 seconds)
+        ''').strip()
+    )
+
     options = parser.add_argument_group(title='Options')
     options.add_argument(
         '-h', '--help',
--- src/ui/console/commands/sync.py
+++ src/ui/console/commands/sync.py
@@ -40,24 +40,6 @@ def create_parser(parser):
         help='exclude pattern (rsync-like), can be specified multiple times'
     )
 
-    historic_group = parser.add_argument_group(
-        title='Backup history management'
-    )
-    historic_group.add_argument(
-        '--max-age', metavar='AGE',
-        default='1w',
-        help=textwrap.dedent('''
-            Max snapshots age.
-            Examples:
-             - 30 (30 days)
-             - 2d10h (2 days 10 hours)
-             - 10m (invalid! cant distinguish between months and minutes)
-             - 10mi (10 minutes)
-             - 10mo (10 months)
-             - 10s1h (stupid form, but valid: 1 hour 10 seconds)
-        ''').strip()
-    )
-
     options = parser.add_argument_group(title='Options')
     options.add_argument(
         '-h', '--help',
